71b2ea8115f2dcf34120ac0a80dac9b9b75f55b7
Skyler Layne <skylerl@indellient.com>
2016-09-30 17:01:37 -0400

    setup spring boot app

--- src/main/java/me/skylerlayne/App.java
+++ src/main/java/me/skylerlayne/App.java
@@ -1,13 +1,25 @@
 package me.skylerlayne;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
 /**
- * Hello world!
+ * API Entry Point
+ * 
+ * @author Skyler Layne
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SuppressWarnings("deprecation")
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
