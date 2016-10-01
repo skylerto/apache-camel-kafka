f8a3db12461b5340c257c6d6dfd66b763fc43ff5
Skyler Layne <skylerl@indellient.com>
2016-10-01 19:41:31 -0400

    add POST for sending Notifications

--- src/main/java/me/skylerlayne/controller/MessageController.java
+++ src/main/java/me/skylerlayne/controller/MessageController.java
@@ -1,5 +1,6 @@
 package me.skylerlayne.controller;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,17 +9,23 @@ import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import me.skylerlayne.model.Notification;
 
 @Controller
 @ResponseBody
 @RequestMapping("/message")
 public class MessageController {
+
+	ObjectMapper mapper = new ObjectMapper();
 
 	@Autowired
 	CamelContext camelContext;
@@ -36,22 +43,47 @@ public class MessageController {
 
 	ConsumerTemplate kafkaConsumer;
 
+	@PostConstruct
+	public void setup() {
+		try {
+			camelContext.addRoutes(kafkaRouteProducer);
+			camelContext.addRoutes(kafkaRouteConsumer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * GET to produce a message for Kafaka.
 	 * 
 	 * @param request
-	 *            - the {@link HttpServletRequest} object.
+	 *            the {@link HttpServletRequest} object.
 	 * @param response
-	 *            - the {@link HttpServletResponse} object.
+	 *            the {@link HttpServletResponse} object.
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public void get(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			camelContext.addRoutes(kafkaRouteProducer);
-			camelContext.addRoutes(kafkaRouteConsumer);
+			kafkaProducer.sendBody("direct:kafkaRoute", "This is a message from the /message route!");
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
 
-			kafkaProducer.sendBody("direct:kafkaRoute", "testKafkaMessage");
-
+	/**
+	 * POST a notification, send it as a message to Kafka.
+	 * 
+	 * @param request
+	 *            the {@link HttpServletRequest} object.
+	 * @param response
+	 *            the {@link HttpServletResponse} object.
+	 * @param notification
+	 *            the {@link Notification} to be posted.
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public void post(HttpServletRequest request, HttpServletResponse response, @RequestBody Notification notification) {
+		try {
+			kafkaProducer.sendBody("direct:kafkaRoute", mapper.writeValueAsString(notification));
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
